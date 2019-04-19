692daa200454905dfaf24967273a98fce778eea2
Eric Johnson <ericwj2@illinois.edu>
2019-04-18 22:40:59 -0500

    end of models, part 3

--- plantjournal/models.py
+++ plantjournal/models.py
@@ -5,15 +5,34 @@ class Light(models.Model):
     light_id = models.AutoField(primary_key=True)
     light_type = models.CharField(max_length=45, unique=True)
 
+    def __str__(self):
+        return '%s' % self.light_type
+
+    class Meta:
+        ordering = ['light_type']
+
 
 class Soil(models.Model):
     soil_id = models.AutoField(primary_key=True)
     soil_type = models.CharField(max_length=45, unique=True)
 
+    def __str__(self):
+        return '%s' % self.soil_type
+
+    class Meta:
+        ordering = ['soil_type']
+
 
 class Humidity(models.Model):
     humidity_id = models.AutoField(primary_key=True)
     humidity_level = models.CharField(max_length=45, unique=True)
+
+    def __str__(self):
+        return '%s' % self.humidity_level
+
+    class Meta:
+        ordering = ['humidity_level']
+        verbose_name_plural = 'humidities'
 
 
 class Water(models.Model):
@@ -21,27 +40,65 @@ class Water(models.Model):
     water_freq = models.CharField(max_length=45)
     water_type = models.CharField(max_length=45, blank=True, default='')
 
+    def __str__(self):
+        if self.water_type == '':
+            return '%s' % self.water_freq
+        else:
+            return '%s (%s)' % (self.water_freq, self.water_type)
+
+    class Meta:
+        ordering = ['water_freq', 'water_type']
+        unique_together = (('water_freq', 'water_type'),)
+
 
 class Fertilizer(models.Model):
     fertilizer_id = models.AutoField(primary_key=True)
     fertilizer_freq = models.CharField(max_length=45)
     fertilizer_type = models.CharField(max_length=45, blank=True, default='')
 
+    def __str__(self):
+        if self.fertilizer_type == '':
+            return '%s' % self.fertilizer_freq
+        else:
+            return '%s (%s)' % (self.fertilizer_freq, self.fertilizer_type)
+
+    class Meta:
+        ordering = ['fertilizer_freq', 'fertilizer_type']
+        unique_together = (('fertilizer_freq', 'fertilizer_type'),)
+
 
 class Toxicity(models.Model):
     toxicity_id = models.AutoField(primary_key=True)
-    toxicity_description = models.TextField()
+    toxicity_type = models.CharField(max_length=45, unique=True)
+
+    def __str__(self):
+        return '%s' % self.toxicity_type
+
+    class Meta:
+        ordering = ['toxicity_type']
+        verbose_name_plural = 'toxicities'
 
 
 class Location(models.Model):
     location_id = models.AutoField(primary_key=True)
     location_name = models.CharField(max_length=45, unique=True)
 
+    def __str__(self):
+        return '%s' % self.location_name
+
+    class Meta:
+        ordering = ['location_name']
+
 
 class Flower(models.Model):
     flower_id = models.AutoField(primary_key=True)
     flower_type = models.CharField(max_length=45, unique=True)
-    flower_date_last = models.DateField(blank=True, default='')
+
+    def __str__(self):
+        return '%s' % self.flower_type
+
+    class Meta:
+        ordering = ['flower_type']
 
 
 class Plant(models.Model):
@@ -51,14 +108,35 @@ class Plant(models.Model):
     plant_latin_name = models.CharField(max_length=100, blank=True, default='')
     plant_nickname = models.CharField(max_length=45, blank=True, default='')
     plant_description = models.TextField(blank=True, default='')
-    light = models.ForeignKey(Light, related_name='plants', on_delete=models.PROTECT, blank=True, default='')
-    soil = models.ForeignKey(Soil, related_name='plants', on_delete=models.PROTECT, blank=True, default='')
-    humidity = models.ForeignKey(Humidity, related_name='plants', on_delete=models.PROTECT, blank=True, default='')
-    water = models.ForeignKey(Water, related_name='plants', on_delete=models.PROTECT, blank=True, default='')
-    fertilizer = models.ForeignKey(Fertilizer, related_name='plants', on_delete=models.PROTECT, blank=True, default='')
-    toxicity = models.ForeignKey(Toxicity, related_name='plants', on_delete=models.PROTECT, blank=True, default='')
-    location = models.ForeignKey(Location, related_name='plants', on_delete=models.PROTECT, blank=True, default='')
-    flower = models.ForeignKey(Flower, related_name='plants', on_delete=models.PROTECT, blank=True, default='')
+    light = models.ForeignKey(Light, related_name='plants', on_delete=models.PROTECT, blank=True, null=True, default='')
+    soil = models.ForeignKey(Soil, related_name='plants', on_delete=models.PROTECT, blank=True, null=True, default='')
+    humidity = models.ForeignKey(Humidity, related_name='plants', on_delete=models.PROTECT, blank=True, null=True,
+                                 default='')
+    water = models.ForeignKey(Water, related_name='plants', on_delete=models.PROTECT, blank=True, null=True, default='')
+    fertilizer = models.ForeignKey(Fertilizer, related_name='plants', on_delete=models.PROTECT, blank=True, null=True,
+                                   default='')
+    toxicity = models.ForeignKey(Toxicity, related_name='plants', on_delete=models.PROTECT, blank=True, null=True,
+                                 default='')
+    location = models.ForeignKey(Location, related_name='plants', on_delete=models.PROTECT, blank=True, null=True,
+                                 default='')
+    flower = models.ForeignKey(Flower, related_name='plants', on_delete=models.PROTECT, blank=True, null=True,
+                               default='')
+
+    def __str__(self):
+        if self.plant_number == '':
+            if self.plant_nickname == '':
+                return '%s' % self.plant_name
+            else:
+                return '%s (%s)' % (self.plant_name, self.plant_nickname)
+        else:
+            if self.plant_nickname == '':
+                return '%s %s' % (self.plant_name, self.plant_number)
+            else:
+                return '%s %s (%s)' % (self.plant_name, self.plant_number, self.plant_nickname)
+
+    class Meta:
+        ordering = ['plant_name', 'plant_number', 'plant_nickname']
+        unique_together = (('plant_name', 'plant_number', 'plant_nickname'),)
 
 
 class Note(models.Model):
@@ -68,13 +146,33 @@ class Note(models.Model):
     note_body = models.TextField()
     plant = models.ForeignKey(Plant, related_name='notes', on_delete=models.PROTECT)
 
+    def __str__(self):
+        return '%s - %s' % (self.note_name, self.note_date)
+
+    class Meta:
+        ordering = ['-note_date', 'note_name']
+
 
 class GrowthType(models.Model):
     growth_type_id = models.AutoField(primary_key=True)
-    growth_type_name = models.CharField(max_length=45)
+    growth_type_name = models.CharField(max_length=45, unique=True)
+
+    def __str__(self):
+        return '%s' % self.growth_type_name
+
+    class Meta:
+        ordering = ['growth_type_name']
 
 
 class GrowthInstance(models.Model):
     growth_instance_id = models.AutoField(primary_key=True)
+    growth_instance_date = models.DateField()
     plant = models.ForeignKey(Plant, related_name='growth_instances', on_delete=models.PROTECT)
     growth_type = models.ForeignKey(GrowthType, related_name='growth_instances', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return '%s - %s (%s)' % (self.plant, self.growth_type, self.growth_instance_date)
+
+    class Meta:
+        ordering = ['-growth_instance_date', 'plant', 'growth_type']
+        unique_together = (('plant', 'growth_type', 'growth_instance_date'),)
